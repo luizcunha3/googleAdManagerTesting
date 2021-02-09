a661d19e25138fd0d26bbc2ca154ea954acec093
Luiz <luiz.cunha.3@gmail.com>
2021-02-09 17:20:25 -0300

    Vai

--- GoogleAdManagerTesting/ViewController.swift
+++ GoogleAdManagerTesting/ViewController.swift
@@ -1,7 +1,34 @@
 import UIKit
 import GoogleMobileAds
 
-class ViewController: UIViewController {
+protocol ViewControllerDelegate {
+    func addBanner(_ banner: GADNativeCustomTemplateAd)
+}
+
+class AdUnitDelegate: UIViewController, GADNativeCustomTemplateAdLoaderDelegate {
+    var delegate: ViewControllerDelegate!
+    
+    func adLoader(_ adLoader: GADAdLoader, didFailToReceiveAdWithError error: GADRequestError) {
+        print("@LUIZ - \(error.localizedDescription)")
+    }
+    
+    func nativeCustomTemplateIDs(for adLoader: GADAdLoader) -> [String] {
+        return ["11965647"]
+    }
+    
+    func adLoader(_ adLoader: GADAdLoader, didReceive nativeCustomTemplateAd: GADNativeCustomTemplateAd) {
+        //DAR JEITO DE PASSAR IMAGEM
+        print("@LUIZ - CHEGOU UM BANNER")
+        delegate.addBanner(nativeCustomTemplateAd)
+    }
+}
+
+class ViewController: UIViewController, ViewControllerDelegate{
+    func addBanner(_ banner: GADNativeCustomTemplateAd) {
+        self.ads.append(banner)
+        print("@LUIZ - \(self.ads.count)")
+    }
+    
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var deeplinkUrl: UILabel!
@@ -13,12 +40,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let option = GADNativeAdImageAdLoaderOptions()
-        option.shouldRequestMultipleImages = true
-        let adUnit = "/22158792083/teste_ios_simples"
-        self.adLoader = GADAdLoader(adUnitID: adUnit, rootViewController: nil, adTypes: [GADAdLoaderAdType.nativeCustomTemplate], options: [option])
-        self.adLoader.delegate = self
-        self.adLoader.load(DFPRequest())
+        let adUnits = ["/22158792083/app_nativo/home/carrossel_1/banner_1","/22158792083/teste_ios_simples","/22158792083/teste_ios_simples"]
+        let adLoader0 = GADAdLoader(adUnitID: adUnits[0], rootViewController: nil, adTypes: [GADAdLoaderAdType.nativeCustomTemplate], options: [])
+        let adLoader1 = GADAdLoader(adUnitID: adUnits[1], rootViewController: nil, adTypes: [GADAdLoaderAdType.nativeCustomTemplate], options: [])
+        let adLoader2 = GADAdLoader(adUnitID: adUnits[2], rootViewController: nil, adTypes: [GADAdLoaderAdType.nativeCustomTemplate], options: [])
+        
+        let delegate0 = AdUnitDelegate()
+        delegate0.delegate = self
+        let delegate1 = AdUnitDelegate()
+        delegate0.delegate = self
+        let delegate2 = AdUnitDelegate()
+        delegate0.delegate = self
+        
+        adLoader0.delegate = delegate0
+        adLoader1.delegate = delegate1
+        adLoader2.delegate = delegate2
+        
+        adLoader0.load(DFPRequest())
+        adLoader1.load(DFPRequest())
+        adLoader2.load(DFPRequest())
     }
     
     @IBAction func clickButton(_ sender: Any) {
@@ -43,6 +83,7 @@ extension ViewController: GADNativeCustomTemplateAdLoaderDelegate {
         self.imageView.image = nativeCustomTemplateAd.image(forKey: "imagem1")!.image
         nativeCustomTemplateAd.delegate = self
         nativeCustomTemplateAd.recordImpression()
+        
     }
 }
 
